1e2eddcf71de51f8b06fd424696b45c51ba485fa
Noah Franco <noah@macbooks-mbp.lan>
2020-04-30 22:27:16 -0400

    added the load funciton so we can pull up files

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,37 +13,45 @@ class CPU:
     def ram_read(self, address):
         # should accept the address to read and return the value stored there.
         self.ram.append(address)
-        return addressOne
+        return address
 
     def ram_write(self, address, value):
         #should accept a value to write, and the address to write it to.
         self.ram[address] = value
 
-    def load(self):
-
-        address = 0
+    def load(self, filename):
 
         HLT = 0b00000001
         PRN = 0b01000111
         LDI = 0b10000010
 
-
         memory = [
-            # From print8.ls8
-            LDI, # LDI R0,8
+            LDI,
             0b00000000,
             0b00001000,
-            PRN, # PRN R0
+            PRN,
             0b00000000,
-            HLT, # HLT
+            HLT,
         ]
 
         for instruction in memory:
             self.ram[address] = instruction
             address += 1
 
+        #  load an .ls8 file given the filename passed in as an argument
+        address = 0
+        with open(filename) as file:
+            for line in file:
+                file_split = line.split("#")
+                number_string = comment_split[0].strip()
 
-# Day Three Below
+                if number_string == "":
+                    continue
+
+                num = int(number_string)
+                memory[address] = num
+                address += 1
+
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -53,6 +61,8 @@ class CPU:
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+
 
     def trace(self):
         """
